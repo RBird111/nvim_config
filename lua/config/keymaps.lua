9226ee4c3b1f3f637152cfde227a72ed22e49d5d
Roosevelt Burden <rooseveltburden@gmail.com>
2024-02-09 12:01:14 -0700

    good starting point

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- This file is automatically loaded by lazyvim.config.init
+local Util = require("lazyvim.util")
+local map = vim.keymap.set
+
+-- better up/down
+map({ "n" }, "<C-d>", "<C-d>zz", { silent = true })
+map({ "n" }, "<C-u>", "<C-u>zz", { silent = true })
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  "navarasu/onedark.nvim",
+  config = function()
+    require("onedark").setup({
+      -- Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+      style = "darker",
+      transparent = true,
+      lualine = {
+        transparent = true, -- lualine center bar transparency
+      },
+    })
+    require("onedark").load()
+  end,
+}
