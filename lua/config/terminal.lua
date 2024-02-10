23505b4b51389cebc7da9ff43807ed4b61551fed
Roosevelt Burden <rooseveltburden@gmail.com>
2024-02-10 13:10:30 -0700

    righteous

--- lua/config/terminal.lua
+++ lua/config/terminal.lua
@@ -6,23 +6,26 @@ local go = function()
   return "go run ."
 end
 
+local ft_cmds = {
+  py = "py " .. vim.fn.expand("%"),
+  js = "node " .. vim.fn.expand("%"),
+  ts = "node " .. vim.fn.expand("%"),
+  rs = "cargo run",
+  ex = "elixir " .. vim.fn.expand("%"),
+  exs = "elixir " .. vim.fn.expand("%"),
+  go = go(),
+}
+
 local M = {}
 
 -- run script
 function M.run_script()
+  vim.cmd.write()
+
   local extension = vim.fn.expand("%:e")
 
-  local ft_cmds = {
-    py = "py " .. vim.fn.expand("%"),
-    js = "node " .. vim.fn.expand("%"),
-    ts = "node " .. vim.fn.expand("%"),
-    rs = "cargo run",
-    ex = "elixir " .. vim.fn.expand("%"),
-    exs = "elixir " .. vim.fn.expand("%"),
-    go = go(),
-  }
-
-  require("nvterm.terminal").send(ft_cmds[extension], "vertical") -- the 2nd argument i.e direction is optional
+  -- horizontal|vertical|float
+  require("nvterm.terminal").send(ft_cmds[extension], "vertical")
 end
 
 return M
