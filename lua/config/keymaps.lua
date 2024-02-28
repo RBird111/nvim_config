80166c17bd44baa838ced7272f75eacb3a1dd999
Roosevelt Burden <rooseveltburden@gmail.com>
2024-02-28 16:45:48 -0700

    misc

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,3 +17,5 @@ map({ "n" }, "<leader>/", "gcc", { silent = true, remap = true })
 
 -- run script (work in progress)
 map({ "n" }, "<leader>rs", require("config.terminal").run_script, { silent = true, desc = "Run script" })
+-- run test
+map({ "n" }, "<leader>rt", require("config.terminal").run_test, { silent = true, desc = "Run test" })
--- lua/config/terminal.lua
+++ lua/config/terminal.lua
@@ -1,11 +1,3 @@
-local go = function()
-  local s = vim.fn.globpath(".", "*")
-  if string.find(s, "./main") then
-    return "go run main/main.go"
-  end
-  return "go run ."
-end
-
 local M = {}
 
 -- run script
@@ -21,7 +13,26 @@ function M.run_script()
     rs = "cargo run",
     ex = "elixir " .. vim.fn.expand("%"),
     exs = "elixir " .. vim.fn.expand("%"),
-    go = go(),
+    go = "go run .",
+  }
+
+  -- horizontal|vertical|float
+  require("nvterm.terminal").send(ft_cmds[extension], "vertical")
+end
+
+function M.run_test()
+  vim.cmd.write()
+
+  local extension = vim.fn.expand("%:e")
+
+  local ft_cmds = {
+    py = "pytest",
+    js = "node " .. vim.fn.expand("%"),
+    ts = "node " .. vim.fn.expand("%"),
+    rs = "cargo test -- --include-ignored",
+    ex = "mix test --include pending",
+    exs = "mix test --include pending",
+    go = "go test -v --bench . --benchmem",
   }
 
   -- horizontal|vertical|float
