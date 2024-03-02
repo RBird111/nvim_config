650d91eece5389f07a5186ae6613e75584eca4c8
Roosevelt Burden <rooseveltburden@gmail.com>
2024-03-02 13:44:33 -0700

    Updated run & test scripts

--- lua/config/terminal.lua
+++ lua/config/terminal.lua
@@ -1,31 +1,36 @@
 local M = {}
 
+M.send = require("nvterm.terminal").send
+
 -- run script
 function M.run_script()
+  -- save file
   vim.cmd.write()
 
   local extension = vim.fn.expand("%:e")
+  local cmd_pre = "cd " .. vim.fn.expand("%:p:h") .. " && "
 
-  local ft_cmds = {
+  local run_cmds = {
     py = "python3 " .. vim.fn.expand("%"),
     js = "bun " .. vim.fn.expand("%"),
     ts = "bun " .. vim.fn.expand("%"),
     rs = "cargo run",
     ex = "elixir " .. vim.fn.expand("%"),
     exs = "elixir " .. vim.fn.expand("%"),
-    go = "go run .",
+    go = M.go_cmd(),
   }
 
-  -- horizontal|vertical|float
-  require("nvterm.terminal").send(ft_cmds[extension], "vertical")
+  M.send(cmd_pre .. run_cmds[extension], "vertical")
 end
 
 function M.run_test()
+  -- save file
   vim.cmd.write()
 
   local extension = vim.fn.expand("%:e")
+  local cmd_pre = "cd " .. vim.fn.expand("%:p:h") .. " && "
 
-  local ft_cmds = {
+  local test_cmds = {
     py = "pytest",
     js = "node " .. vim.fn.expand("%"),
     ts = "node " .. vim.fn.expand("%"),
@@ -35,8 +40,21 @@ function M.run_test()
     go = "go test -v --bench . --benchmem",
   }
 
-  -- horizontal|vertical|float
-  require("nvterm.terminal").send(ft_cmds[extension], "vertical")
+  M.send(cmd_pre .. test_cmds[extension], "vertical")
+end
+
+function M.go_cmd()
+  local directory = vim.fn.expand("%:p:h")
+  local contents = vim.split(vim.fn.glob(directory .. "/**"), "\n", { trimempty = true })
+
+  for _, item in pairs(contents) do
+    local i = string.find(item, "main.go")
+    if i ~= nil then
+      return "go run " .. item
+    end
+  end
+
+  return "echo 'main function not found'"
 end
 
 return M
