395a65ea116f6fad9b31aca54e70d15d37e50b2d
Roosevelt Burden <rooseveltburden@gmail.com>
2024-03-23 10:56:53 -0600

    Removed <C-q> quit shortcut

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,16 @@
 return {
-  "navarasu/onedark.nvim",
-  config = function()
-    require("onedark").setup({
-      -- Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-      style = "cool",
-      transparent = true,
-      lualine = {
-        transparent = true, -- lualine center bar transparency
-      },
-    })
-    require("onedark").load()
-  end,
+  {
+    "navarasu/onedark.nvim",
+    config = function()
+      require("onedark").setup({
+        -- Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+        style = "dark",
+        transparent = true,
+        lualine = {
+          transparent = true, -- lualine center bar transparency
+        },
+      })
+      require("onedark").load()
+    end,
+  },
 }
