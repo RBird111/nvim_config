4232879aba777b791e5a7ed12c1329161056270c
Roosevelt Burden <rooseveltburden@gmail.com>
2024-06-12 19:08:37 -0600

    update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,16 +1,22 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
--- local autocmd = vim.api.nvim_create_autocmd
+local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto resize panes when resizing nvim window
--- autocmd("VimResized", {
---   pattern = "*",
---   command = "tabdo wincmd =",
--- })
+autocmd("VimResized", {
+  pattern = "*",
+  command = "tabdo wincmd =",
+})
 
 -- autocmd("ExitPre", {
 --   group = vim.api.nvim_create_augroup("Exit", { clear = true }),
 --   command = "set guicursor=a:ver90",
 --   desc = "Set cursor back to beam when leaving Neovim.",
 -- })
+
+autocmd({ "BufLeave", "FocusLost" }, {
+  pattern = "*",
+  command = "silent! wall",
+  desc = "Save buffers on focus loss.",
+})
--- lua/plugins/telescope.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-telescope/telescope.nvim",
-  keys = {
-    -- disable the keymap to grep files
-    { "<leader>/", false },
-    -- change a keymap
-    { "<leader>.", require("lazyvim.util").telescope("live_grep"), desc = "Grep (root dir)" },
-  },
-}
